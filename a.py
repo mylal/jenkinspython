a1468ac8bef41cadd0a9a85b17ad6f0de26e204e
sandeep singh <sandeep.ss8258@gmail.com>
2021-03-13 12:42:09 +0530

    Git and Github integration with jenkins

---
+++ a.py
@@ -0,0 +1,3 @@
+#! /bin/bash
+print("Now i am learning intrgration of git and github with jenkins")
+
---
+++ b.py
@@ -0,0 +1,2 @@
+#! /bin/bash
+print("Now my task is research and implement on jenkins with github integration")
